694df962f55b0652b751612705821d7dae6f6877
rafli.haidar30@gmail.com <rafli.haidar30@gmail.com>
2023-03-08 23:12:59 +0700

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import Home from "./components/Home";
 import Surah from "./components/Surah";
 import Audio from "./components/Audio";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes basename="/Alquran-Website">
+    <HashRouter>
+      <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/surah/:surahId" exact element={<Surah />} />
         <Route path="/audio" exact element={<Audio />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
